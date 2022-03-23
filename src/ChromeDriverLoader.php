9e05dd5145d013a26189c9848ae62fab24495994
charles <charleshopkinsiv@gmail.com>
2022-03-22 23:46:22 -0700

    updates

--- src/ChromeDriverLoader.php
+++ src/ChromeDriverLoader.php
@@ -12,7 +12,7 @@ use Facebook\WebDriver\WebDriverExpectedCondition;
 class ChromeDriverLoader
 {
 
-    private static $chromedriver_file = "/home/charles/var/chromedriver";
+    private static $chromedriver_dir = __DIR__ . "/../bin";
     private static $default_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36";
     private static $driver_url_base = "https://chromedriver.storage.googleapis.com/";
     private static $driver_file_name = "chromedriver_linux64.zip";
@@ -21,14 +21,14 @@ class ChromeDriverLoader
     public static function load(string $user_agent = "", string $ip_address = "")
     {
 
-        if(!is_file(__DIR__ . "/../driver_bin/chromedriver"))
+        if(!is_file(self::$chromedriver_dir . "/chromedriver"))
             self::downloadBin();
 
         for($i = 0; $i < 2; $i++) {
 
             try{
 
-                putenv('WEBDRIVER_CHROME_DRIVER=' . self::$chromedriver_file);
+                putenv('WEBDRIVER_CHROME_DRIVER=' . self::$chromedriver_dir . "/chromedriver");
 
                 $user_agent = empty($user_agent) ? self::$default_user_agent : $user_agent;        
                 
@@ -69,9 +69,10 @@ class ChromeDriverLoader
             $Zip = new \ZipArchive;
             if($file = $Zip->open(self::$zip_file)) {
 
-                $Zip->extractTo(__DIR__ . "/../bin");
+                $Zip->extractTo(self::$chromedriver_dir);
                 $Zip->close();
                 unlink(self::$zip_file);
+                chmod(self::$chromedriver_dir . "/chromedriver", 0775);
             }
 
             else {
